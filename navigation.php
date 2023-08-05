86de6eacbfe37d3a77671451ad8affe4a56b168e
John Doe <michalwesolowski11@gmail.com>
2023-08-05 15:25:09 +0100

    add navigation

---
+++ navigation.php
@@ -0,0 +1,51 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <div class="container-fluid">
+            <!-- Logo in the middle -->
+            <a class="navbar-brand mx-auto d-lg-none" href="#">Your Logo</a>
+
+            <!-- Hamburger button for mobile -->
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                <!-- Left-aligned links -->
+                <ul class="navbar-nav-left nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                </ul>
+
+                <!-- Logo in the middle (visible on larger screens) -->
+                <a class="navbar-brand d-none d-lg-block" href="#">Your Logo</a>
+
+                <!-- Right-aligned links -->
+                <ul class="navbar-nav-right nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn" href="#">Book a Table</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
