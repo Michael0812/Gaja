a56a8c09d56394954bccad98f9052f1eb2d9d503
John Doe <michalwesolowski11@gmail.com>
2023-08-15 18:20:11 +0100

    remove some extra p element

--- functions.php
+++ functions.php
@@ -13,3 +13,4 @@ function enqueue_custom_styles() {
     wp_enqueue_style('custom-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+remove_filter('the_content', 'wpautop');
