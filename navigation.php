62fd1380bf69759aa22a9dda2e2034675f47ee27
John Doe <michalwesolowski11@gmail.com>
2023-08-23 22:25:03 +0100

    add logo

--- navigation.php
+++ navigation.php
@@ -2,7 +2,9 @@
     <nav class="navbar navbar-expand-lg bg-transparent">
         <div class="container-fluid">
             <!-- Logo in the middle -->
-            <a class="navbar-brand mx-auto d-lg-none" href="#">Your Logo</a>
+            <a class="text-light navbar-brand ml-4 ml-md-0 mx-md-auto d-lg-none" href="#">
+            <img src="/wp-content/themes/Gaja/images/gaja-logo1.svg" alt="Gaja logo" class="logo-mobile">
+            </a>
 
             <!-- Hamburger button for mobile -->
             <button
@@ -13,36 +15,40 @@
                 aria-controls="navbarNav"
                 aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
             </button>
 
             <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
                 <!-- Left-aligned links -->
                 <ul class="navbar-nav-left nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="#about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Menu</a>
+                        <a class="nav-link" href="#menu">Menu</a>
                     </li>
                 </ul>
 
                 <!-- Logo in the middle (visible on larger screens) -->
-                <a class="navbar-brand d-none d-lg-block" href="#">Your Logo</a>
+                <a class="navbar-brand d-none d-lg-block" href="#">
+                    <img src="/wp-content/themes/Gaja/images/gaja-logo3.svg" alt="Gaja logo" class="logo">
+                </a>
 
                 <!-- Right-aligned links -->
                 <ul class="navbar-nav-right nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Gallery</a>
+                        <a class="nav-link step-offset" href="#gallery">Gallery</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
+                        <a class="nav-link" href="#contact-us">Contact Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link btn btn-purple" href="#">Book a Table</a>
+                        <a class="nav-link nav-btn btn btn-purple" href="#">Book a Table</a>
                     </li>
                 </ul>
             </div>
